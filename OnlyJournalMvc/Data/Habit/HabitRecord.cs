8b08b7d47101471d9c3cabb595b801d2b07cb3e2
NumAniCloud <num.ani.cloud@gmail.com>
2020-06-27 15:52:55 +0900

    不要なプロジェクトを削除

--- OnlyJournalMvc/Data/Habit/HabitRecord.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OnlyJournal.Data.Habit
-{
-	public class HabitRecord
-	{
-		public string Key { get; set; }
-		public string Title { get; set; }
-		public int SuccessCount { get; set; }
-	}
-}
--- OnlyJournalMvc/Data/Journal/Journal.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace OnlyJournal.Data.Journal
-{
-	public class Journal
-	{
-		public int ID { get; set; }
-		public string Markdown { get; set; }
-	}
-}
